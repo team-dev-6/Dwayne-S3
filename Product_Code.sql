7b674a1fa122dc35b22f9449b265e7d36e5b126d
Dwayne-capacity <60339764+Dwayne-capacity@users.noreply.github.com>
2020-02-19 13:34:09 +0200

    Add files via upload

---
+++ Product_Code.sql
@@ -0,0 +1,6 @@
+Item_No,Food_Item,Category,Size,Price,Stock_Quantity,Stock_Value,Expiry_Date,Reorder_Time_In_Days,Quantity_In_Reorder,Product_Code
+A002,Lays Salted Potato Chips,Snacks,125g,19.49,11,214.39,2020/11/22,53,125,
+A003,Coca-Cola Original Bottle,Beverages,2L,22.49,6,134.94,2020/09/05,10,200,
+A004,"Fanta Orange Bottle,Beverages",Beverages,2L,22.49,4,89.96,2020/09/13,5,185,
+A005,Cadbury Dairy Milk Chocolate Slab,Chocolates,80g,15.99,19,303.81,2020/06/03,14,85,
+A006,Nestle Quality Street Chocolates,Chocolates,500g,99.99,6,599.94,2020/04/28,61,50,
