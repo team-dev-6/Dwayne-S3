54303a048c426818d3b94bd181e5ce144daa7df1
Dwayne-capacity <60339764+Dwayne-capacity@users.noreply.github.com>
2020-02-19 13:29:38 +0200

    Delete Product_Code.sql

--- Product_Code.sql
+++
@@ -1,6 +0,0 @@
-Item_No	Food_Item	Category	Size	Price	Stock_Quantity	Stock_Value	Expiry_Date	Reorder_Time_In_Days	Quantity_In_Reorder	Product_Code
-A002	Lays Salted Potato Chips	Snacks	125g	19.49	11	214.39	11/22/2020	53	125	
-A003	Coca-Cola Original Bottle	Beverages	2L	22.49	6	134.94	9/5/2020	10	200	
-A004	Fanta Orange Bottle,Beverages	Beverages	2L	22.49	4	89.96	9/13/2020	5	185	
-A005	Cadbury Dairy Milk Chocolate Slab	Chocolates	80g	15.99	19	303.81	6/3/2020	14	85	
-A006	Nestle Quality Street Chocolates	Chocolates	500g	99.99	6	599.94	4/28/2020	61	50	
